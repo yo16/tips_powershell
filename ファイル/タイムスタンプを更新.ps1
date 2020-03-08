c0b92c3ef53411d64b7053d93c7ba1bca8e97e10
yo16 <xxyo1xx@gmail.com>
2020-03-08 13:53:35 +0900

    add EditFileProperty

---
+++ ファイル/タイムスタンプを更新.ps1
@@ -0,0 +1,8 @@
+# 作成日時を変更
+$FileName = "sample.xlsx"
+Set-ItemProperty $FileName -Name CreationTime -Value "1999/9/9 09:09:09"
+
+# 更新日時を変更
+Set-ItemProperty $FileName -Name LastWriteTime -Value "2001/2/3 04:05:06"
+
+pause
